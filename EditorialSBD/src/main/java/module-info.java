0847b1a730422f2f215f91c0d8492a177606a34a
BryanEstrada003 <mbestrad@espol.edu.ec>
2022-12-30 03:00:51 -0500

    hice cosas pero ando dormido que no se que hice

--- EditorialSBD/src/main/java/module-info.java
+++ EditorialSBD/src/main/java/module-info.java
@@ -1,7 +1,10 @@
 module com.mycompany.editorialsbd {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
+    requires java.base;
+    requires java.desktop;
+    requires java.logging;
     opens com.mycompany.editorialsbd to javafx.fxml;
     exports com.mycompany.editorialsbd;
 }
